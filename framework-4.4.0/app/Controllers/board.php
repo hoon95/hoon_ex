3e69677ffc7cb1403aff9df1eed165fdaa0f625a
hoon95 <gmline_@naver.com>
2023-09-12 10:33:58 +0900

    Add file upload

--- framework-4.4.0/app/Controllers/board.php
+++ framework-4.4.0/app/Controllers/board.php
@@ -1,7 +1,10 @@
 <?php
 
 namespace App\Controllers;
+
 use App\Models\BoardModel;      // 사용할 모델을 로드
+use App\Models\FileModel;
+
 use CodeIgniter\I18n\Time;      // 날짜 라이브러리
 class Board extends BaseController
 {
@@ -56,8 +59,9 @@ class Board extends BaseController
         // $formattedTime = $myTime -> toDateTimeString();
 
         // $sql = "INSERT INTO board (userid, subject, content, regdate) VALUES('test', ?,?,?)";
-        // $result = $db -> query($sql,[$subject, $content, $formattedTime]);   
+        // $result = $db -> query($sql,[$subject, $content, $formattedTime]);
         $boardModel = new BoardModel();
+        $fileModel = new FileModel();
 
         $myTime = new Time('now', 'Asia/Seoul');
         $myTime ->modify('+9 hours');     
@@ -70,12 +74,23 @@ class Board extends BaseController
         ];
         
         $bid = $this->request->getVar('bid');
+        $file = $this->request->getFile('upfile');
 
-        if($bid){
+        $db = db_connect();     // 새 글 등록 시 자동으로 file_table의 fid 넣을 id 생성하기 위해
+
+        if($file->getName()){
+            $filename = $file->getName();
+            $newFile = $file->getRandomeName();
+            $filepath = $file->store('board/', $newName);
+        }
+
+        if($bid){       // 수정
             $boardModel ->update($bid, $data);
             return $this->response ->redirect(site_url('/boardview/'.$bid)); //쿼리성공후 board 페이지로 이동
-        }else{
+        }else{          // 신규 글 등록
             $boardModel ->insert($data);
+
+            $FileModel -> insert($fileData);
             return $this->response ->redirect(site_url('/board')); //쿼리성공후 board 페이지로 이동
         }
 
@@ -109,6 +124,14 @@ class Board extends BaseController
 
     }
     public function delete($bid = null){
+        $boardModel = new BoardModel();
+        $board = $boardModel->find($bid);
 
+        if($_SESSION['userid'] == $board->userid){
+            $boardModel->delete($bid);
+            return redirect()->to('/board');
+        }else{
+            return redirect()->to('/board')->with('alert','본인 글만 삭제할 수 있습니다');
+        }
     }
 }
--- framework-4.4.0/app/Views/board_write.php
+++ framework-4.4.0/app/Views/board_write.php
@@ -13,6 +13,14 @@
       <label for="content" class="form-label">내용</label>
       <textarea class="form-control" name="content" id="content" rows="3"><?= isset($view->content)? $view->content:''; ?></textarea>
     </div>
-    <button class="btn btn-primary">등록</button>
+    <div class="mb-3">
+      <label for="file" class="form-label">파일</label>
+      <input type="file" class="form-control" name="upfile" id="file">
+    </div>
+    <?php
+    $btntitle = isset($view->subject)? '수정':'등록';
+    
+    ?>
+    <button class="btn btn-primary"><?= $btntitle; ?></button>
 </form>
 <hr>
