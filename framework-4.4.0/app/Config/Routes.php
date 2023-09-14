34e0dcd4a769bb35b66152492a3b8ed290b3e524
hoon95 <gmline_@naver.com>
2023-09-14 12:19:27 +0900

    ajax 다중파일 첨부

--- framework-4.4.0/app/Config/Routes.php
+++ framework-4.4.0/app/Config/Routes.php
@@ -4,7 +4,7 @@ use CodeIgniter\Router\RouteCollection;
 
 /**
  * @var RouteCollection $routes
-*/
+ */
 $routes->get('/', 'Home::index');
 $routes->get('/board', 'Board::list');
 $routes->get('/board/write', 'Board::write');
--- framework-4.4.0/app/Controllers/board.php
+++ framework-4.4.0/app/Controllers/board.php
@@ -4,31 +4,28 @@ namespace App\Controllers;
 use App\Models\BoardModel; //사용할 모델을 로드
 use App\Models\FileModel; //사용할 모델을 로드
 use CodeIgniter\I18n\Time;
-use CodeIgniter\Pager\Pager;
+use CodeIgniter\Pager\Pager;  
 
 class Board extends BaseController
 {
     public function list(): string
     {
-        // return view('board_list');
-        // $db = db_connect();
-        // $sql = "SELECT * from board order by bid desc";
-        // $result = $db -> query($sql);
-        // $data['list'] = $result->getResult(); //fetch_object 글 결과 배열에 담기
         $boardModel = new BoardModel();
-        $page = $this -> request -> getVar('page') ?? 1;
-        $perPage = 3;
-        $startLimit = ($page-1) * $perPage;
+        $page = $this->request->getVar('page') ?? 1;
+        $perPage = 10;
+        $startLimit = ($page-1)*$perPage;
 
-        $list = $boardModel -> select('*') -> where('1=1') -> orderBy('bid', 'desc') -> limit($perPage, $startLimit)  -> findAll($perPage, $startLimit);
+        $list = $boardModel -> select('*') -> where('1=1') -> orderBy('bid', 'desc')->limit($perPage, $startLimit)->findAll($perPage, $startLimit);
 
         $total = $boardModel -> countAllResults();
 
+        
         $pager = service('pager');
-        $pager_links = $pager->makeLinks($page, $perPage, $total, 'default_full');
+
+        $pager_links = $pager->makeLinks($page, $perPage, $total,'default_full');
 
         $data = [
-            'total' => $total,
+            'total' =>  $total,
             'page' => $page,
             'perPage' => $perPage,
             'pager_links' => $pager_links,
@@ -36,6 +33,7 @@ class Board extends BaseController
         ];
 
         return render('board_list' ,$data);
+
     }
     public function write()
     {
@@ -101,7 +99,7 @@ class Board extends BaseController
 
         if($bid){ //수정
             $boardModel ->update($bid,$data);        
-            // $insertid= $db->insertID();      //board테이블에 글등록후 생기는 고유id를 생성
+            //$insertid= $db->insertID();//board테이블에 글등록후 생기는 고유id를 생성
 
             foreach($filepath as $fp){
                 $fileData=[
@@ -172,40 +170,45 @@ class Board extends BaseController
             return redirect()->to('/board')->with('alert','본인글만 삭제할 수 있습니다.');  
         }
 
-    }
+    }   
     public function save_image(){
-        // 파일 정보 파악
         $fileModel = new FileModel();
         $db = db_connect();
-        $file = $this->request->getFile('savefile');
 
-        // 파일명 랜덤 생성 및 이동(store)
+        $file = $this->request->getFile('savefile'); 
+
         if($file->getName()){
             $filename=$file->getName();
             $newName = $file ->getRandomName();
             $filepath = $file->store('board/', $newName);
         }
 
-        // file_table에서는 bid를 받을 방법이 없음(fid : 12,13,14 / bid : 0,0,0)
-        // save 함수 실행(글 등록)시 fid에 해당하는 bid 매칭됨
         $fileData=[
             'bid' => '',
             'userid' => $_SESSION['userid'],
             'filename' => $filepath,
             'type' => 'board'
         ];
-
-        $fileModel = insert($fileData);
-        $insertid = $db->insertID();        // id를 생성하는 것이 아닌 이미 만들어진 id를 조회하는 것
+        $fileModel ->insert($fileData);
+        $insertid = $db->insertID();
 
         $return_data = array(
-            'result' => 'success',
-            'fid' => $insertid,
+            'result'=>'success',
+            'fid' =>  $insertid,
             'savename' => $filepath
         );
-
         return json_encode($return_data);
-        // 기존 : 파일 자체에 실행(레거시)
-        // 변경 : 주소로 넘긴 값을 컨트롤러 함수로 실행 후 return
+
+    }
+    public function file_delete(){
+        $fileModel = new FileModel();
+        $fid = $this->request->getVar('fid'); 
+        $file = $fileModel -> where('fid', $fid)->first();
+
+        unlink('uploads/'.$file->filename);//서버에서 해당파일 삭제
+        $fileModel -> where('fid', $fid)->delete();//테이블에서 행 삭제
+        
+        $return_data = array('result' => 'ok');
+        return json_encode($return_data);
     }
 }
