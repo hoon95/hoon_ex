582ee27ebaaec9c3f4405dc4ef5a2ab5b49b5881
hoon95 <gmline_@naver.com>
2023-09-18 14:24:17 +0900

    start laravel-first-app

---
+++ laravel/laravel-first-app/routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/about', [HomeController::class, 'about']);
+Route::get('/contact', [HomeController::class, 'contact']);
+
+Route::get('/store/{category}/{item}', function ($category=null, $item=null) {
+
+    if(isset($cateogry)){
+			if(isset($item)){
+        return '현재 스토어에서'.strip_tags($category).'내'.$item.'카테고리를  보고 있습니다';
+	    }else{
+				return '현재 스토어에서'.strip_tags($category).'카테고리를  보고 있습니다';
+			}
+		}else{
+	    return '현재 스토어에서 모든 제품을 보고 있습니다';
+		}
+});
