f2017ec9d1e1c649dd85d2721d7e148de20b6ada
hoon95 <gmline_@naver.com>
2023-09-19 10:47:02 +0900

    Add BikeController

--- laravel/laravel-first-app/routes/web.php
+++ laravel/laravel-first-app/routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\BikeController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,12 +16,13 @@ use App\Http\Controllers\HomeController;
 |
 */
 
-Route::get('/', [HomeController::class, 'index']);
-Route::get('/about', [HomeController::class, 'about']);
-Route::get('/contact', [HomeController::class, 'contact']);
+Route::get('/', [HomeController::class, 'index']) -> name('home.index');
+Route::get('/about', [HomeController::class, 'about']) -> name('home.about');
+Route::get('/contact', [HomeController::class, 'contact']) -> name('home.contact');
+
+Route::resource('bikes', BikeController::class);
 
 Route::get('/store/{category}/{item}', function ($category=null, $item=null) {
-
     if(isset($cateogry)){
 			if(isset($item)){
         return '현재 스토어에서'.strip_tags($category).'내'.$item.'카테고리를  보고 있습니다';
