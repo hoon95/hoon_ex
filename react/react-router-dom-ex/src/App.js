12c6a265a439b6a2ca08b9aac8e3a170e704b6e6
hoon95 <gmline_@naver.com>
2023-10-04 16:11:48 +0900

    Update router in params

--- react/react-router-dom-ex/src/App.js
+++ react/react-router-dom-ex/src/App.js
@@ -1,5 +1,11 @@
 import './App.css';
-import { Routes, Route, Link, NavLink } from "react-router-dom";
+import { Routes, Route, Link, NavLink, useParams } from "react-router-dom";
+
+let contents = [
+  {id:1, title:'HTML', desc:'HTML is..'},
+  {id:2, title:'JS', desc:'JS is..'},
+  {id:3, title:'React', desc:'React is..'} 
+]
 
 function Home(){
   return(
@@ -14,40 +20,61 @@ function Home(){
   )
 }
 function Topics(){
+  let list = [];
+  list = contents.map(content=>{
+    return <li key={content.id}><NavLink to={"/topics/"+content.id}>{content.title}</NavLink></li>
+  })
   return(
     <div>
       <h2>Topics</h2>
       <p>Topic is...</p>
       <nav>
         <ul className="main-menu">
-          <li><NavLink to="/topics/1">HTML</NavLink></li>
-          <li><NavLink to="/topics/2">CSS</NavLink></li>
-          <li><NavLink to="/topics/3">JavaScript</NavLink></li>
+          {list}
+          {/* <li><NavLink to="/topics/1">HTML</NavLink></li> */}
         </ul>
       </nav>
       <Routes>
-        <Route path="1" element={<Topic />}/>
-        <Route path="2" element={<Topic />}/>
-        <Route path="3" element={<Topic />}/>
+        <Route path=":topic_id" element={<Topic />}/>
       </Routes>
     </div>
   )
 }
-let contents = [
-  {id:1, title:'HTML', description:'HTML is..'},
-  {id:2, title:'JS', description:'JS is..'},
-  {id:3, title:'React', description:'React is..'} 
-]
+
 function Topic(){
-  let list = [];
+  let params = useParams();
+  let topic_id = params.topic_id;
+  let selected_topic = {
+    title: 'Title is none',
+    desc: '404 Not Found'
+  }
+
+  // for문 활용
+  // for(let i=0; i<contents.length; i++){
+  //   if(contents[i].id === Number(params.topic_id)){
+  //     selected_topic = contents[i];
+  //     break;
+  //   }
+  // }
+
+  // find 활용
+  // function getContent(topic_id){
+  //   return contents.find(content=>    // return 값이 하나인 경우 괄호 및 return 생략
+  //     content.id === Number(topic_id)
+  //   )
+  // }
+
+  selected_topic = contents.find(content=>
+    content.id === Number(topic_id)
+  )
+
+
   return(
     <div>
-      <h3>Topic</h3>
-      <p>Topic is...?</p>
+      <h3>{selected_topic.title}</h3>
+      <p>{selected_topic.desc}</p>
       <nav>
         <ul className='main-menu'>
-          {/* <li><NavLink to="/topics/1">HTML</NavLink></li> */}
-          {list}
         </ul>
       </nav>
     </div>
@@ -79,7 +106,7 @@ function App() {
       </nav>
       <Routes>
         <Route path="/" element={<Home />}/>
-        <Route path="/topics" element={<Topics />}/>
+        <Route path="/topics/*" element={<Topics />}/>
         <Route path="/contact" element={<Contact />}/>
       </Routes>
     </div>
