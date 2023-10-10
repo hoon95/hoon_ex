6f2d168e07cb6bc296033304945e7cea729f39e0
hoon95 <gmline_@naver.com>
2023-10-10 11:29:38 +0900

    server update

--- react/react-board-server/index.js
+++ react/react-board-server/index.js
@@ -2,23 +2,23 @@ const cors = require('cors');
 const express = require('express');
 const app = express();
 const port = process.env.port || 8000;
-const mysql  = require('mysql');
+const mysql = require('mysql');
 
 app.use(express.json()); //json은 말 그대로 JSON 형태의 데이터 전달을 의미하고, 
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
 let corsOptions = {
   origin: '*',
-  credentials:true //사용자 인증에 필요한 리소스 허용
+  credentials: true //사용자 인증에 필요한 리소스 허용
 }
 app.use(cors(corsOptions));
 
 
 const db = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'react_bbs',
-  password : '12345',
-  database : 'react_bbs'
+  host: 'localhost',
+  user: 'react_bbs',
+  password: '12345',
+  database: 'react_bbs'
 });
 
 db.connect();
@@ -26,7 +26,7 @@ db.connect();
 app.get('/', (req, res) => {
   //res.send('Hello World!');
   const sqlQuery = "INSERT INTO requested (rowno) values (1)";
-  db.query(sqlQuery, function(err, result) {
+  db.query(sqlQuery, function (err, result) {
     if (err) throw err;
     res.send('success');
   });
@@ -34,20 +34,31 @@ app.get('/', (req, res) => {
 app.get('/list', (req, res) => {
   //res.send('Hello World!');
   const sqlQuery = "SELECT BOARD_ID, BOARD_TITLE, REGISTER_ID, DATE_FORMAT(REGISTER_DATE, '%Y-%m-%d') AS REGISTER_DATE  FROM board";
-  db.query(sqlQuery, function(err, result) {
+  db.query(sqlQuery, function (err, result) {
     if (err) throw err;
     res.send(result);
   });
 });
+app.get('/detail', (req, res) => {
+  const id = req.query.id;
+  const sqlQuery = "SELECT BOARD_ID, BOARD_TITLE, BOARD_CONTENT, REGISTER_ID, DATE_FORMAT(REGISTER_DATE, '%Y-%m-%d') AS REGISTER_DATE  FROM board WHERE BOARD_ID=?";
+  db.query(sqlQuery, [id], function (err, result) {
+    if (err) throw err;
+    res.send(result);
+    console.log(result);
+  });
+})
+
+
 app.post('/insert', (req, res) => {
   // const title = req.body.title;
   // const content = req.body.content;
-  const {title, content} = req.body;
+  const { title, content } = req.body;
   console.log(title, content);
 
   const sqlQuery = "INSERT INTO board (BOARD_TITLE, BOARD_CONTENT, REGISTER_ID) values (?, ?,'admin')";
 
-  db.query(sqlQuery,[title,content], function(err, result) {
+  db.query(sqlQuery, [title, content], function (err, result) {
     if (err) throw err;
     res.send(result);
   });
