4373805f59105ba33172e18844587e9547d87d28
hoon95 <gmline_@naver.com>
2023-10-12 11:42:57 +0900

    Add collection, docs into store

--- firebase/src/firebase.js
+++ firebase/src/firebase.js
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -28,3 +30,4 @@ export const app = initializeApp(firebaseConfig);
 
 // Initialize Firebase Authentication and get a reference to the service
 export const authService = getAuth(app);
+export const db = getFirestore(app);
--- firebase/src/routes/Home.js
+++ firebase/src/routes/Home.js
@@ -1,5 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { db } from '../firebase';
+import { collection, addDoc, serverTimestamp } from "firebase/firestore";
 
-const Home = ()=><h2>Home Page</h2>
+const Home = () => {
+  const [post, setPost] = useState('');
+  const onChange = (e) => {
+    // const val = e.target.value;    // ES2012
+    const { target: { value } } = e;  // ES2016
+    setPost(value);
+  }
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    // Add a new document with a generated id.
+    const docRef = await addDoc(collection(db, "posts"), {
+      date: serverTimestamp(),
+      post: post
+    });
+    console.log("Document written with ID: ", docRef.id);
+
+  }
+  console.log(post);
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input type='text' value={post} placeholder='제목을 입력하세요' onChange={onChange}></input>
+        <input type='submit' value='작성완료'></input>
+      </form>
+    </div>
+  )
+}
 
 export default Home;
