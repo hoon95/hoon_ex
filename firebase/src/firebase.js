a205d59b5539b33fa1f404b3add10b3438b29c24
hoon95 <gmline_@naver.com>
2023-10-12 13:27:07 +0900

    Update firebase env key

--- firebase/src/firebase.js
+++ firebase/src/firebase.js
@@ -8,18 +8,18 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  // apiKey: process.env.REACT_APP_APIKEY,
-  // authDomain: process.env.REACT_APP_AUTHDOMAIN,
-  // projectId: process.env.REACT_APP_PROJECTID,
-  // storageBucket: process.env.REACT_APP_STORAGEBUCKET,
-  // messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
-  // appId: process.env.REACT_APP_APPID
-  apiKey: "AIzaSyC2FHfN6K9HbG1IyyxHur2Vnh-wN2a7dh4",
-  authDomain: "react-bbs-a53c8.firebaseapp.com",
-  projectId: "react-bbs-a53c8",
-  storageBucket: "react-bbs-a53c8.appspot.com",
-  messagingSenderId: "625471420892",
-  appId: "1:625471420892:web:22ac7066fe2cbd4e3eb719"
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_APPID
+  // apiKey: "AIzaSyC2FHfN6K9HbG1IyyxHur2Vnh-wN2a7dh4",
+  // authDomain: "react-bbs-a53c8.firebaseapp.com",
+  // projectId: "react-bbs-a53c8",
+  // storageBucket: "react-bbs-a53c8.appspot.com",
+  // messagingSenderId: "625471420892",
+  // appId: "1:625471420892:web:22ac7066fe2cbd4e3eb719"
 };
 
 // Initialize Firebase
