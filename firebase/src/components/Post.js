bc39e378eb1e5fb0d454202d9e711ae0c37b5d6b
hoon95 <gmline_@naver.com>
2023-10-13 10:45:14 +0900

    update, delete

--- firebase/src/components/Post.js
+++ firebase/src/components/Post.js
@@ -1,16 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
+import { db } from "../firebase";
+import { doc, updateDoc, deleteDoc } from "firebase/firestore";
 
-const Post = ({ postObj, userConfirm }) => (
-  <li>
-    <h4>{postObj.post}</h4>
-    {userConfirm && (
-      <>
-        <button>Edit</button>
-        <button>Delete</button>
-      </>
-    )
+const Post = ({ postObj, userConfirm }) => {
+  const [edit, setEdit] = useState(false);
+  const [newPost, setNewPost] = useState(postObj.post);
+
+  // edit 버튼 클릭 시 write mode, 취소 버튼 클릭 시 Read mode
+  const toggleEditMode = () => setEdit((prev) => !prev);
+
+  // 변경 된 value값을 change이벤트를 통해 업데이트
+  const onChange = (e) => {
+    // setNewPost(e.target.value);
+    const { target: { value } } = e;
+    setNewPost(value);
+  }
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    await updateDoc(doc(db, "posts", postObj.id), {
+      post: newPost
+    });
+    setEdit(false);
+  }
+
+  const deletePost = async () => {
+    if (window.confirm('정말 삭제하시겠습니까?')) {
+      await deleteDoc(doc(db, "posts", postObj.id));
     }
-  </li>
-)
+  }
 
+  return (
+    <li>
+      {edit ? (
+        <>
+          <form onSubmit={onSubmit}>
+            <input value={newPost} onChange={onChange} required></input>
+            <button>Update</button>
+          </form>
+          <button onClick={toggleEditMode}>cancel</button>
+        </>
+      )
+        : (
+          <>
+            <h4>{postObj.post}</h4>
+            {
+              userConfirm && (
+                <>
+                  <button onClick={toggleEditMode}>Edit</button>
+                  <button onClick={deletePost}>Delete</button>
+                </>
+              )
+            }
+          </>
+        )
+      }
+    </li>
+  )
+}
 export default Post;
